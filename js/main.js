52ee462aade43ab5a4959d152b11be51c998021b
Aleksandr <85560442+Barsez@users.noreply.github.com>
2023-09-05 13:41:19 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".menu_logo img").on("click", function() {
+  $(".menu").toggleClass("menu_hidden");
+  $(".container-fluid").toggleClass("container2");
+})
+
+$(".menu_close").on("click", function() {
+  $(".menu").toggleClass("menu_hidden");
+  $(".container-fluid").toggleClass("container2");
+})
